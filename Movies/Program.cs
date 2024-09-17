0626d12f0be5a3a7d96c48b8dddb606ac954b787
Dimitri <dkakhniashvili@DKAKHNIASHVILI>
2024-09-17 16:41:39 +0400

    Add project files.

---
+++ Movies/Program.cs
@@ -0,0 +1,9 @@
+﻿using Movies;
+
+Movie_Class avengers = new("The Avengers", "Rejisori1", "PG-13");
+//Movie_Class avengers = new Movie_Class("The Avengers", "Rejisori1", "PG-13");
+
+Movie_Class shrek = new("Shrek", "Rejisori2", "PG");
+
+
+Console.WriteLine(shrek.title + " " + shrek.director + " " + shrek.rating);
---
+++ Movies/class/Movie_Class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies
+{
+    class Movie_Class
+    {
+        public string title;
+        public string director;
+        public string rating;
+
+        public Movie_Class(string title, string director, string rating)
+        {
+            this.title = title;
+            this.director = director;
+            this.rating = rating;
+        }
+
+        //public Movie_Class(string aTitle, string aDirector, string aRating)
+        //{
+        //    title = aTitle;
+        //    director = aDirector;
+        //    rating = aRating;
+        //}
+    }
+}

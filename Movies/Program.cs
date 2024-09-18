62a5b039f56ea08684b79d294e259dec8cbb35de
Dimitri <dkakhniashvili@DKAKHNIASHVILI>
2024-09-18 17:51:12 +0400

    2

--- Movies/Program.cs
+++ Movies/Program.cs
@@ -6,10 +6,14 @@
 
 var shrek = new Movie_Class("Shrek", "Rejisori2", "PG");
 
-shrek.Rating = "ADWFAWF";
+//shrek.Rating = "ADWFAWF";
+
+
+var supernatural = new Series_SubClass("supernatural", "Rejisori1", "PG-13", 450);
 
 
 Console.WriteLine(shrek.title + " " + shrek.director + " " + shrek.Rating);
 Console.WriteLine(Movie_Class.movieCount);
+
 
 
---
+++ Movies/class/Series_SubClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies
+{
+    class Series_SubClass : Movie_Class
+    {
+        public int seriesCount;
+
+        //public Series_SubClass(string title, string director, string rating, int seriesCount)
+        //   : base(title, director, rating)
+        //{
+        //    this.seriesCount = seriesCount;
+        //}
+
+
+        public Series_SubClass(string title, string director, string rating, int seriesCount)
+           : base(title, director, rating) => this.seriesCount = seriesCount;
+
+    }
+
+ 
+}
